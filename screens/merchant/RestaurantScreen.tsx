5248742926d49e91eb897f26e48b0a50052b6c0e
Jyan Zarate <git@jcharante.com>
2023-08-28 21:00:40 +0700

    working image upload prototype

--- screens/merchant/RestaurantScreen.tsx
+++ screens/merchant/RestaurantScreen.tsx
@@ -45,10 +45,10 @@ export const RestaurantScreen = ({ navigation, route  }) => {
 
         console.log(pickerResult)
 
-        if (pickerResult.cancelled === true) return
+        if (pickerResult.canceled === true) return
 
         const img = await manipulateAsync(
-            pickerResult.uri,
+            pickerResult.assets[0].uri,
             [{ resize: { width: 500 } }],
             { format: SaveFormat.JPEG, compress: 1, base64: false }
         )
@@ -56,57 +56,38 @@ export const RestaurantScreen = ({ navigation, route  }) => {
         console.log(img)
 
 
-        const { url, fields } = await postImageMutation.mutateAsync({ restaurantID })
-        console.log({ url, fields })
+        const url = await postImageMutation.mutateAsync({ restaurantID })
+        console.log({ url })
 
         const localUri = img.uri;
         const fileExtension = localUri.split('.').pop();
-        const mimeType = `image/${fileExtension}`;
+        const mimeType = `image/${fileExtension === 'jpg' ? 'jpeg' : fileExtension}`;
 
-        // Create a FormData instance
-        const formData = new FormData();
-
-        // Append the fields to the FormData instance
-        for (const field in fields) {
-            formData.append(field, fields[field]);
-        }
 
         // Append the file to the FormData instance
         if (Platform.OS === 'android') {
-            const fetchResponse = await fetch(localUri);
-            const blob = await fetchResponse.blob();
-            formData.append('file', blob, `${fields.key}.${fileExtension}`);
         } else {
-            const fileData = await FileSystem.readAsStringAsync(localUri, {
-                encoding: FileSystem.EncodingType.Base64,
+            const response = await fetch(localUri);
+            const blob = await response.blob();
+
+
+            const uploadResponse = await fetch(url, {
+                method: 'PUT',
+                body: blob,
+                headers: {
+                    'Content-Type': mimeType,
+                },
             });
 
-            const binaryData = base64.decode(fileData);
-            const arrayBuffer = new Uint8Array(binaryData.length);
-            for (let i = 0; i < binaryData.length; i++) {
-                arrayBuffer[i] = binaryData.charCodeAt(i);
+            if (uploadResponse.ok) {
+                console.log('Image uploaded successfully');
+            } else {
+                const errorText = await uploadResponse.text();
+                console.error('Error uploading image', errorText);
             }
-            const blob = new Blob([arrayBuffer], { type: mimeType });
-
-            formData.append('file', blob, `${fields.key}.${fileExtension}`);
-        }
-
-        // Upload the image using the URL and FormData
-        const response = await fetch(url, {
-            method: 'POST',
-            body: formData,
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'multipart/form-data',
-            },
-        });
-
-        if (response.ok) {
-            console.log('Image uploaded successfully');
-        } else {
-            console.error('Error uploading image', response);
         }
     };
+
 
     return <View padding-10>
         <Text>Restaurant Name: {getName(shop.names, 'en')}</Text>
