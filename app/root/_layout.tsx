9620cd688d731a5d7cc95b48e4c3efd89e4a5f9e
Jyan Zarate <git@jcharante.com>
2023-08-28 21:00:41 +0700

    import neutral colors from figma, remove magic numbers in tab bar setup and use filled in outlines when tab active

--- app/root/_layout.tsx
+++ app/root/_layout.tsx
@@ -1,15 +1,24 @@
 import { Slot, Tabs, Stack } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons';
 
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../tailwind.config.js'
+import {tw} from "../../util/utilities";
+
+// @ts-ignore
+const twConfig = resolveConfig(tailwindConfig)
+
 export default function HomeLayout() {
-    return <Tabs>
+    return <Tabs screenOptions={{ tabBarStyle: tw`rounded-tl-2xl rounded-tr-2xl`, tabBarItemStyle: tw`p-1`}}>
         <Tabs.Screen name="home/index" options={{
             tabBarLabel: 'Home',
             tabBarIcon: ((props: {
                 focused: boolean;
                 color: string;
                 size: number;
-            }) => <Ionicons name="home-outline" size={24} color={props.focused ? '#2AC3EC' : 'black'} />)
+            }) => <Ionicons name={props.focused ? 'home' : 'home-outline'}
+                            size={24}
+                            color={props.focused ? twConfig.theme.colors.primary[600] : twConfig.theme.colors.neutral[600]} />)
         }}  />
         <Tabs.Screen name="orders/index" options={{
             title: 'Orders',
@@ -18,7 +27,9 @@ export default function HomeLayout() {
                 focused: boolean;
                 color: string;
                 size: number;
-            }) => <Ionicons name="document-text-outline" size={24} color={props.focused ? '#2AC3EC' : 'black'} />)
+            }) => <Ionicons name={props.focused ? 'document-text' : 'document-text-outline'}
+                            size={24}
+                            color={props.focused ? twConfig.theme.colors.primary[600] : twConfig.theme.colors.neutral[600]} />)
         }}  />
         <Tabs.Screen name="help/index" options={{
             title: 'Help',
@@ -27,7 +38,9 @@ export default function HomeLayout() {
                 focused: boolean;
                 color: string;
                 size: number;
-            }) => <Ionicons name="chatbubbles-outline" size={24} color={props.focused ? '#2AC3EC' : 'black'} />)
+            }) => <Ionicons name={props.focused ? 'chatbubbles' : "chatbubbles-outline"}
+                            size={24}
+                            color={props.focused ? twConfig.theme.colors.primary[600] : twConfig.theme.colors.neutral[600]} />)
         }}  />
         <Tabs.Screen name="me/index" options={{
             title: 'Me',
@@ -36,7 +49,9 @@ export default function HomeLayout() {
                 focused: boolean;
                 color: string;
                 size: number;
-            }) => <Ionicons name="person-circle-outline" size={24} color={props.focused ? '#2AC3EC' : 'black'} />)
+            }) => <Ionicons name={props.focused ? 'person-circle' : "person-circle-outline"}
+                            size={24}
+                            color={props.focused ? twConfig.theme.colors.primary[600] : twConfig.theme.colors.neutral[600]} />)
         }}  />
     </Tabs>
 }
