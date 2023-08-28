49cff83b1e0dacf2193373db343558158066a20f
Jyan Zarate <git@jcharante.com>
2023-08-28 21:00:40 +0700

    generate metro config and add watcher for local api instance

---
+++ metro.config.js
@@ -0,0 +1,18 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+// Absolute path to your package
+const packagePath =
+    '/Users/jyan/Documents/api';
+
+
+module.exports = getDefaultConfig(__dirname);
+
+module.exports = {
+    ...module.exports,
+    resolver: {
+        ...module.exports.resolver,
+        nodeModulesPath: [packagePath]
+    },
+    watchFolders: [packagePath]
+}
